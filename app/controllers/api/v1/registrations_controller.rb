1aeaa1fbf14eb3ba6b11b8201a4c5ffd2e9786a0
fyurchik <jyrafenun@gmail.com>
2024-09-11 18:57:02 +0300

    Make a user registration

---
+++ app/controllers/api/v1/registrations_controller.rb
@@ -0,0 +1,18 @@
+class Api::V1::RegistrationsController < ApplicationController
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      render json: { success: "Saved" }, status: :ok
+    else
+      render json: { error: @user.errors.messages.map { |msg, desc|
+      msg.to_s + " " + desc[0] }.join(",") }, status: :unauthorized
+    end
+  end
+
+  private
+  
+  def user_params
+    params.require(:users).permit(:name, :email, :password,
+    :password_confirmation)
+  end
+end
